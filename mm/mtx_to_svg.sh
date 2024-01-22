bb0f6ecf14b5a72ac8c0ce7a0d5564696358e285
Peiming Liu <peiming@google.com>
2024-01-22 22:35:01 +0000

    update symetric mtx

--- mm/mtx_to_svg.sh
+++ mm/mtx_to_svg.sh
@@ -4,8 +4,9 @@ for MAT in $ms;
 do
 
 exec 5< $MAT
+sym=false
 while read line <&5; do
-  sym=false
+
   if [[ $line == %* ]]; then
     if [[ $line == *symmetric* ]]; then
       sym=true
@@ -20,7 +21,8 @@ echo "<svg width=\"${crd[1]}\" height=\"${crd[0]}\" version=\"1.1\" xmlns=\"http
 while read line <&5; do
   crd=( $line )
   echo "<rect x=\"${crd[1]}\" y=\"${crd[0]}\" width=\"1\" height=\"1\"/>" >> ${MAT}.svg
-  if $sym; then
+
+  if [[ $sym = true ]]; then
     echo "<rect x=\"${crd[0]}\" y=\"${crd[1]}\" width=\"1\" height=\"1\"/>" >> ${MAT}.svg
   fi
 done
