bc5f07bc0518e75e9a3a065aad4e4ba8e3f32c5c
Peiming Liu <peiming@google.com>
2024-01-22 22:25:47 +0000

    fix svg

--- mm/mtx_to_svg.sh
+++ mm/mtx_to_svg.sh
@@ -5,18 +5,24 @@ do
 
 exec 5< $MAT
 while read line <&5; do
+  sym=false
   if [[ $line == %* ]]; then
-    echo $line
+    if [[ $line == *symmetric* ]]; then
+      sym=true
+    fi
   else
     break
   fi
 done
 
 crd=( $line )
-echo "<svg width=\"${crd[0]}\" height=\"${crd[1]}\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">" > ${MAT}.svg
+echo "<svg width=\"${crd[1]}\" height=\"${crd[0]}\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">" > ${MAT}.svg
 while read line <&5; do
   crd=( $line )
-  echo "<rect x=\"${crd[0]}\" y=\"${crd[1]}\" width=\"1\" height=\"1\"/>" >> ${MAT}.svg
+  echo "<rect x=\"${crd[1]}\" y=\"${crd[0]}\" width=\"1\" height=\"1\"/>" >> ${MAT}.svg
+  if $sym; then
+    echo "<rect x=\"${crd[0]}\" y=\"${crd[1]}\" width=\"1\" height=\"1\"/>" >> ${MAT}.svg
+  fi
 done
 echo "</svg>" >> ${MAT}.svg
 
